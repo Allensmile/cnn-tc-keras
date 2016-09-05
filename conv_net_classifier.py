3167f12bff249eb286a23f6b0a029afb6d713c2e
vivounicorn <zhangleisuper@gmail.com>
2016-09-05 15:05:17 +0800

    new

--- conv_net_classifier.py
+++ conv_net_classifier.py
@@ -3,35 +3,186 @@ import numpy as np
 from collections import defaultdict
 import sys, re
 import pandas as pd
+import math
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D
 import conv_net_dataset
 from conv_net_dataset import CnnDataSet
+from sklearn import metrics
 
-if __name__=="__main__":
-    print "args: max_length train-filepath test-filepath dataset-filepath"
+def build_common_cnn(img_rows, img_cols):
+    nb_classes = 2
+
+    # number of convolutional filters to use
+    nb_filters = 64 #32
+    # size of pooling area for max pooling
+    nb_pool = 2 
+    # convolution kernel size
+    kernel_size = (2,2)
+
+    model = Sequential()
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                            border_mode='same',
+                            input_shape=(img_rows, img_cols, 1), dim_ordering='tf'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], dim_ordering='tf'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), strides=(2, 2), dim_ordering='tf'))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax')) 
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+    return model
+ 
+def build_vgg16(img_rows, img_cols):
+    nb_classes = 2
+    # build the VGG16 network
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(img_rows, img_cols, 1), dim_ordering='tf'))
+    first_layer = model.layers[-1]
+    # this is a placeholder tensor that will contain our generated images
+    input_img = first_layer.input
+
+    # build the rest of the network
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2', dim_ordering='tf'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2', dim_ordering='tf'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3', dim_ordering='tf'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), dim_ordering='tf'))
+
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3', dim_ordering='tf'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), dim_ordering='tf'))
+  
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2', dim_ordering='tf'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3', dim_ordering='tf'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), dim_ordering='tf'))
+    model.add(Flatten())
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax')) 
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+    
+    return model
+ 
+def run_with_vector():
+    print "args: max_length train-filepath test-filepath"
     cf = CnnDataSet()
     print "loading training data..."
     max_l = int(sys.argv[1])
     x = cPickle.load(open(sys.argv[2],"rb"))
     revs, cf.W, cf.W2, cf.word_idx_map, cf.vocab = x[0], x[1], x[2], x[3], x[4]
     print "data loaded!"
-    trainX, trainY, validateX, validateY = cf.make_cross_data_cv(revs, cf.W, 10, max_l)
+
+    U = cf.W2
+    head_filter = 2
+    cv = 10
+    trainX, trainY, validateX, validateY = cf.make_cross_data_cv(revs, U, cv, max_l, head_filter)
     print "loading testing data..."
     x = cPickle.load(open(sys.argv[3],"rb"))
     revs = x[0]
     print "data loaded!"
-    testX, testY = cf.make_test_data(revs, cf.W, max_l)
+    testX, testY = cf.make_test_data(revs, U, max_l, head_filter)
+
+    batch_size = 256
+    nb_epoch = 10
+
+    # input image dimensions
+    img_rows, img_cols = cf.nrows, len(U[0])
+    nb_classes = 2
+
+    print '----',len(trainX),' ',trainX.shape[0],' ',img_rows,' ',img_cols
+    trainX = trainX.reshape(trainX.shape[0], img_rows, img_cols, 1)
+    validateX = validateX.reshape(validateX.shape[0], img_rows, img_cols, 1)
+    testX = testX.reshape(testX.shape[0], img_rows, img_cols, 1)
+    trainX = trainX.astype('float32')
+    validateX = validateX.astype('float32')
+    testX = testX.astype('float32')
+    print('trainX shape:', trainX.shape)
+    print(trainX.shape[0], 'train samples')
+    print(validateX.shape[0], 'validate samples')
+    print(testX.shape[0], 'test samples')
+
+    trainY = np_utils.to_categorical(trainY, nb_classes)
+    validateY = np_utils.to_categorical(validateY, nb_classes)
+    testY = np_utils.to_categorical(testY, nb_classes)
+
+    model = build_common_cnn(img_rows, img_cols)  #  build_vgg16(img_rows, img_cols)#
+    model.fit(trainX, trainY, batch_size=batch_size, nb_epoch=nb_epoch,
+              verbose=1, validation_data=(validateX, validateY))
+    score = model.evaluate(testX, testY, verbose=0)
+    predY = model.predict_classes(testX, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+    confusion = metrics.confusion_matrix(testY[:,1], predY)
+    TP = confusion[1, 1]
+    TN = confusion[0, 0]
+    FP = confusion[0, 1]
+    FN = confusion[1, 0]
+    print 'Confusion matrix:', confusion
+    print 'Accuracy:(TP+TN) / float(TP+TN+FN+FP)=', (TP+TN) / float(TP+TN+FN+FP)
+    print 'Error:(FP+FN) / float(TP+TN+FN+FP)=', (FP+FN) / float(TP+TN+FN+FP)
+    print 'Recall:TP / float(TP+FN)=',TP / float(TP+FN)
+    print 'Specificity:TN / float(TN+FP)=', TN / float(TN+FP)
+    print 'False Positive Rate:FP / float(TN+FP)=', FP / float(TN+FP)
+    print 'Precision:TP / float(TP+FP)=', TP / float(TP+FP)
+    print 'AUC=',metrics.roc_auc_score(testY[:,1], predY)
+
+
+def run_with_id():
+    print "args: max_length train-filepath test-filepath dataset-filepath"
+    cf = CnnDataSet()
+    print "loading training data..."
+    max_l = 2 * int(sys.argv[1])
+    head_filter = 10
+    x = cPickle.load(open(sys.argv[2],"rb"))
+    revs, cf.W, cf.W2, cf.word_idx_map, cf.vocab = x[0], x[1], x[2], x[3], x[4]
+    print "data loaded!"
+    trainX, trainY, validateX, validateY = cf.make_cross_id_data_cv(revs, 5, max_l, head_filter)
+    print "loading testing data..."
+    x = cPickle.load(open(sys.argv[3],"rb"))
+    revs = x[0]
+    print "data loaded!"
+    testX, testY = cf.make_test_id_data(revs, max_l, head_filter)
 
 
     batch_size = 256
     nb_classes = 2
-    nb_epoch = 5
+    nb_epoch = 15
 
     # input image dimensions
-    img_rows, img_cols = cf.nrows, lwn(cf.W[0])
+    img_rows, img_cols = 12, 10
     # number of convolutional filters to use
     nb_filters = 64
     # size of pooling area for max pooling
@@ -39,7 +190,7 @@ if __name__=="__main__":
     # convolution kernel size
     kernel_size = (3, 3)
 
-    print '----',len(trainX),' ',trainX.shape[0],' ',img_rows,' ',img_cols
+    print '----',len(trainX[0]),' ',trainX.shape[0],' ',img_rows,' ',img_cols
     trainX = trainX.reshape(trainX.shape[0], img_rows, img_cols, 1)
     validateX = validateX.reshape(validateX.shape[0], img_rows, img_cols, 1)
     testX = testX.reshape(testX.shape[0], img_rows, img_cols, 1)
@@ -64,20 +215,29 @@ if __name__=="__main__":
     model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.3))
 
     model.add(Flatten())
     model.add(Dense(128))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax')) 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adadelta',
+                  optimizer='adagrad',
                   metrics=['accuracy'])
     
     model.fit(trainX, trainY, batch_size=batch_size, nb_epoch=nb_epoch,
               verbose=1, validation_data=(validateX, validateY))
     score = model.evaluate(testX, testY, verbose=0)
+    predY = model.predict_classes(testX, verbose=0)
+    for i in range(0,len(predY)):
+        print 'real:',testY[i],' pred:',predY[i]
+
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
+    print '',  metrics.accuracy_score(testY[:,1], predY)
+    print metrics.confusion_matrix(testY[:,1], predY)
+
+if __name__=="__main__":
+    run_with_vector()
--- process_data.py
+++ process_data.py
@@ -1,169 +1,189 @@
 import numpy as np
 import cPickle
 from collections import defaultdict
-import sys, re
+import sys, re, os
 import pandas as pd
 
-class DataPrepare:
-
-    vecdim = 200
-    revs = []
-    W = []
-    W2 = []
-    word_idx_map = dict()
-    vocab = defaultdict(float)
-
-    def __init__(self):
-        pass
-
-    def read_file(self,data_folder, cv=10, clean_string=True):
-        sp = data_folder.split(":")
-        if len(sp) != 2:
-            print "[read_file] file name format error."
+def read_file(revs, vocab, data_folder, cv=10, clean_string=False):
+    sp = data_folder.split(":")
+    if len(sp) != 2:
+        print "[read_file] file name format error." + data_folder
+        return 
+    fname = sp[0]
+    label = sp[1]
+    if not os.path.exists(fname):
+            print "[read_file] error file not exists."+  fname
             return 
-        with open(sp[0], "rb") as f:
-           for line in f:       
-               rev = []
-               rev.append(line.strip())
-               if clean_string:
-                   orig_rev = clean_str(" ".join(rev))
-               else:
-                   orig_rev = " ".join(rev).lower()
-               words = set(orig_rev.split())
-               for word in words:
-                   self.vocab[word] += 1
-               datum  = {"y":sp[1], 
-                         "text": orig_rev,                             
-                         "num_words": len(orig_rev.split()),
-                         "split": np.random.randint(0,cv)}
-               self.revs.append(datum)
-        
-    def build_data_cv(self, data_files, cv=10, clean_string=True):
-        """
-        Loads data and split into 10 folds. data_folder=["file1:0","file2:1","file3:2"]
-        """
-        pos_file = data_files[0]
-        neg_file = data_files[1]
-        self.read_file(pos_file, cv, clean_string)
-        self.read_file(neg_file, cv, clean_string)
-    
-    def get_W(self, word_vecs, freed=0):
-        """
-        Get word matrix. W[i] is the vector for word indexed by i
-        """
-        if freed == 0:
-            freed = self.vecdim
-        vocab_size = len(word_vecs)
-        self.W = np.zeros(shape=(vocab_size+1, self.vecdim), dtype='float32')            
-        self.W2 = np.zeros(shape=(vocab_size+1, freed), dtype='float32')            
-        self.W[0] = np.zeros(self.vecdim, dtype='float32')
-        self.W2[0] = np.zeros(freed, dtype='float32')
-        i = 1
-        for word in word_vecs:
-            self.W[i] = word_vecs[word]
-            self.W2[i] = np.random.uniform(-0.25,0.25,freed)
-            self.word_idx_map[word] = i
-            i += 1
-    
-    def load_bin_vec(self, fname):
-        """
-        Loads nx1 word vecs from Google (Mikolov) word2vec
-        """
-        word_vecs = {}
-        with open(fname, "rb") as f:
-            header = f.readline()
-            vocab_size, layer1_size = map(int, header.split())
-            binary_len = np.dtype('float32').itemsize * layer1_size
-            for line in xrange(vocab_size):
-                word = []
-                while True:
-                    ch = f.read(1)
-                    if ch == ' ':
-                        word = ''.join(word)
-                        break
-                    if ch != '\n':
-                        word.append(ch)   
-                word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')  
-        self.vecdim = layer1_size
-        return word_vecs
 
-    def load_txt_vec(self, fname):
-        """
-        Loads nx1 word vecs from Google (Mikolov) word2vec
-        """
-        word_vecs = {}
-        with open(fname, "rb") as f:
-            header = f.readline()
-            vocab_size, layer1_size = map(int, header.split())
+    with open(fname, "rb") as f:
+       for line in f:       
+           rev = []
+           rev.append(line.strip())
+           if clean_string:
+               orig_rev = clean_str(" ".join(rev))
+           else:
+               orig_rev = " ".join(rev).lower()
+           words = set(orig_rev.split())
+           for word in words:
+               vocab[word] += 1
+           datum  = {"y":label, 
+                     "text": orig_rev,                             
+                     "num_words": len(orig_rev.split()),
+                     "split": np.random.randint(0,cv)}
+           revs.append(datum)
+
+def build_data_cv(data_files, cv=10, clean_string=False):
+    """
+    Loads data and split into 10 folds.
+    """
+    revs = []
+    vocab = defaultdict(float)
+    for dfile in data_files:
+        read_file(revs, vocab, dfile, cv, clean_string)
+    return revs, vocab
+ 
+def get_W(word_vecs, k=300):
+    """
+    Get word matrix. W[i] is the vector for word indexed by i
+    """
+    vocab_size = len(word_vecs)
+    word_idx_map = dict()
+    W = np.zeros(shape=(vocab_size+1, k), dtype='float32')            
+    W[0] = np.zeros(k, dtype='float32')
+    i = 1
+    for word in word_vecs:
+        W[i] = word_vecs[word]
+        word_idx_map[word] = i
+        i += 1
+    return W, word_idx_map
+
+def load_bin_vec(fname, vocab):
+    """
+    Loads 300x1 word vecs from Google (Mikolov) word2vec
+    """
+    word_vecs = {}
+    layer1_size = 0
+    if not os.path.exists(fname):
+        print "[load_bin_vec] error file not exists." + fname
+        return word_vecs, layer1_size
+
+    with open(fname, "rb") as f:
+        header = f.readline()
+        vocab_size, layer1_size = map(int, header.split())
+        binary_len = np.dtype('float32').itemsize * layer1_size
+        for line in xrange(vocab_size):
             word = []
             while True:
-                lines = f.readlines(100000)
-                if not lines:
-                     break    
-                for line in lines:
-                     sp = line.split()
-                     word_vecs[sp[0]] = np.array(map(float, sp[1:]),dtype='float32')  
-                     if len(word_vecs[sp[0]]) != layer1_size:
+                ch = f.read(1)
+                if ch == ' ':
+                    word = ''.join(word)
+                    break
+                if ch != '\n':
+                    word.append(ch)   
+            if word in vocab:
+               word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')  
+            else:
+                f.read(binary_len)
+    return word_vecs, layer1_size
+
+def load_txt_vec(fname, vocab):
+    """
+    Loads nx1 word vecs from Google (Mikolov) word2vec
+    """
+    word_vecs = {}
+    layer1_size = 0
+    if not os.path.exists(fname):
+        print "[load_txt_vec] error file not exists." + fname
+        return word_vecs, layer1_size
+
+    with open(fname, "rb") as f:
+        header = f.readline()
+        vocab_size, layer1_size = map(int, header.split())
+        word = []
+        while True:
+            lines = f.readlines(100000)
+            if not lines:
+                 break    
+            for line in lines:
+                 sp = line.split()
+                 word = sp[0]
+                 if word in vocab:
+                     word_vecs[word] = np.array(map(float, sp[1:]),dtype='float32')  
+                     if len(word_vecs[word]) != layer1_size:
                          print "[load_txt_vec] word vector error."
-        self.vecdim = layer1_size
-        return word_vecs
+                     else:
+                         continue
+    return word_vecs, layer1_size
 
+def add_unknown_words(word_vecs, vocab, k=300, min_df=1):
+    """
+    For words that occur in at least min_df documents, create a separate word vector.    
+    0.25 is chosen so the unknown vectors have (approximately) same variance as pre-trained ones
+    """
+    i = 0
+    for word in vocab:
+        if word not in word_vecs and vocab[word] >= min_df:
+            word_vecs[word] = np.random.uniform(-0.25,0.25,k)
+            i += 1
+            if i % 100 ==0:
+                print i,' unknown word is added such as ', word,'.'  
 
-    def add_unknown_words(self, word_vecs, min_df=1):
-        """
-        For words that occur in at least min_df documents, create a separate word vector.    
-        0.25 is chosen so the unknown vectors have (approximately) same variance as pre-trained ones
-        """ 
-        for word in self.vocab:
-            if word not in word_vecs or self.vocab[word] < min_df:
-                word_vecs[word] = np.random.uniform(-0.25,0.25,self.vecdim)  
+def clean_str(string, TREC=False):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Every dataset is lower cased except for TREC
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
+    string = re.sub(r"\'s", " \'s", string) 
+    string = re.sub(r"\'ve", " \'ve", string) 
+    string = re.sub(r"n\'t", " n\'t", string) 
+    string = re.sub(r"\'re", " \'re", string) 
+    string = re.sub(r"\'d", " \'d", string) 
+    string = re.sub(r"\'ll", " \'ll", string) 
+    string = re.sub(r",", " , ", string) 
+    string = re.sub(r"!", " ! ", string) 
+    string = re.sub(r"\(", " \( ", string) 
+    string = re.sub(r"\)", " \) ", string) 
+    string = re.sub(r"\?", " \? ", string) 
+    string = re.sub(r"\s{2,}", " ", string)    
+    return string.strip() if TREC else string.strip().lower()
 
-    def clean_str(self, string, TREC=False):
-        """
-        Tokenization/string cleaning for all datasets except for SST.
-        Every dataset is lower cased except for TREC
-        """
-        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
-        string = re.sub(r"\'s", " \'s", string) 
-        string = re.sub(r"\'ve", " \'ve", string) 
-        string = re.sub(r"n\'t", " n\'t", string) 
-        string = re.sub(r"\'re", " \'re", string) 
-        string = re.sub(r"\'d", " \'d", string) 
-        string = re.sub(r"\'ll", " \'ll", string) 
-        string = re.sub(r",", " , ", string) 
-        string = re.sub(r"!", " ! ", string) 
-        string = re.sub(r"\(", " \( ", string) 
-        string = re.sub(r"\)", " \) ", string) 
-        string = re.sub(r"\?", " \? ", string) 
-        string = re.sub(r"\s{2,}", " ", string)    
-        return string.strip() if TREC else string.strip().lower()
+def clean_str_sst(string):
+    """
+    Tokenization/string cleaning for the SST dataset
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)   
+    string = re.sub(r"\s{2,}", " ", string)    
+    return string.strip().lower()
 
-    def clean_str_sst(self, string):
-        """
-        Tokenization/string cleaning for the SST dataset
-        """
-        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)   
-        string = re.sub(r"\s{2,}", " ", string)    
-        return string.strip().lower()
-
-if __name__=="__main__":   
-    print "args: w2v-filepath positive-filepath negative-filepath output-filepath dim" 
-    w2v_file = sys.argv[1]     
-    data_files = [sys.argv[2]+":1",sys.argv[3]+":0"]
-    print "loading data..."
-    dp = DataPrepare()
-    dp.build_data_cv(data_files, 10, False)
-    max_l = np.max(pd.DataFrame(dp.revs)["num_words"])
-    print "data loaded!"
-    print "number of sentences: " + str(len(dp.revs))
-    print "vocab size: " + str(len(dp.vocab))
+if __name__=="__main__":
+    if len(sys.argv) != 5:    
+        print "args: w2v-filepath data-filepath[file1:0,file2:1,file3:2] dim output-filepath"
+        sys.exit(-1)
+    w2v_file = sys.argv[1]
+    data_files = sys.argv[2]
+    data_folder = data_files.split(',')
+    k = int(sys.argv[3])
+    fpath = sys.argv[4]
+    print 'w2v-filepath: ', w2v_file
+    print 'data-filepath: ', data_folder
+    print 'dim: ', k
+    print 'output-filepath: ' + fpath
+    print "loading data...\n",        
+    revs, vocab = build_data_cv(data_folder, cv=10, clean_string=False)
+    max_l = np.max(pd.DataFrame(revs)["num_words"])
+    print "data loaded!\n"
+    print "number of sentences: " + str(len(revs))
+    print "vocab size: " + str(len(vocab))
     print "max sentence length: " + str(max_l)
-    print "loading word2vec vectors...",
-    w2v = dp.load_bin_vec(w2v_file)
-    print "word2vec loaded!"
-    print "num words already in word2vec: " , len(w2v)
-    dp.add_unknown_words(w2v)
-    dp.get_W(w2v, int(sys.argv[5]))
-    cPickle.dump([dp.revs, dp.W, dp.W2, dp.word_idx_map, dp.vocab], open(sys.argv[4], "wb"))
+    print "loading word2vec vectors...\n",
+    w2v, _ = load_bin_vec(w2v_file, vocab)
+    print "word2vec loaded!\n"
+    print "num words already in word2vec: " + str(len(w2v))
+    add_unknown_words(w2v, vocab, k)
+    W, word_idx_map = get_W(w2v, k)
+    rand_vecs = {}
+    add_unknown_words(rand_vecs, vocab, k)
+    W2, _ = get_W(rand_vecs, k)
+    cPickle.dump([revs, W, W2, word_idx_map, vocab], open(fpath, "wb"))
     print "dataset created!"
-    

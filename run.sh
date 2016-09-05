94529d526ed2447874a3bcd66632d93e956c38c6
vivounicorn <zhangleisuper@gmail.com>
2016-09-05 15:06:54 +0800

    new

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-#python process_data.py True data/askprice/vectors.bin data/askprice/positive.train data/askprice/negative.train train.p 100
-#python process_data.py True data/askprice/vectors.bin data/askprice/positive.test data/askprice/negative.test test.p 100
 
-python conv_net_classifier.py 51 train.p test.p
+python process_data.py data/askprice/vectors.bin data/askprice/posv2.train:1,data/askprice/negv2.train:0 150 train.p
+
+python process_data.py data/askprice/vectors.bin data/askprice/posv2.test:1,data/askprice/negv2.test:0 150 test.p
+
+python conv_net_classifier.py 70 train.p test.p
 
